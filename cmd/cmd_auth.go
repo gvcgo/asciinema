be6ee491203bf9a83f676802f4db043bbd58d26a
moqsien <moqsien@foxmail.com>
2023-08-03 15:59:03 +0800

    auth

--- cmd/cmd_auth.go
+++ cmd/cmd_auth.go
@@ -13,6 +13,8 @@ var info string = `Open the following URL in a web browser to link your install 
 This will associate all recordings uploaded from this machine (past and future ones) to your account, 
 and allow you to manage them (change title/theme, delete) at https://asciinema.org.`
 
-func (r *Runner) Auth() string {
-	return fmt.Sprintf(info, fmt.Sprintf(Auth_API, cfg.ApiToken()))
+func (r *Runner) Auth() (authUrl, result string) {
+	authUrl = fmt.Sprintf(Auth_API, cfg.ApiToken())
+	result = fmt.Sprintf(info, authUrl)
+	return
 }
