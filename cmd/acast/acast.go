fc9715752b0d1fef8f908e83e664d2f876d1c678
moqsien <moqsien@foxmail.com>
2024-04-06 17:20:12 +0800

    commands

--- cmd/acast/acast.go
+++ cmd/acast/acast.go
@@ -1,1 +1,125 @@
 package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/gvcgo/asciinema/cmd"
+	"github.com/gvcgo/asciinema/util"
+	"github.com/gvcgo/goutils/pkgs/gtea/gprint"
+	"github.com/gvcgo/goutils/pkgs/gutils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	GitTag  string
+	GitHash string
+	GroupID string = "asciinema"
+)
+
+func SetWorkDir() {
+	homeDir, _ := os.UserHomeDir()
+	gvcDir := filepath.Join(homeDir, ".gvc")
+	var workdir string
+	if ok, _ := gutils.PathIsExist(gvcDir); ok {
+		workdir = filepath.Join(gvcDir, "asciinema")
+	} else {
+		workdir = filepath.Join(homeDir, ".config", "asciinema")
+	}
+	os.MkdirAll(workdir, os.ModePerm)
+	os.Setenv(util.DefaultHomeEnv, workdir)
+}
+
+func getName(base string) string {
+	if base == "" {
+		return base
+	}
+	return strings.Split(base, ".")[0]
+}
+
+func handleFilePath(fpath string) (title, result string) {
+	cwd, _ := os.Getwd()
+	if fpath == "" {
+		return "default_cast", filepath.Join(cwd, "default.cast")
+	}
+	base := filepath.Base(fpath)
+	if base == fpath {
+		return getName(base), filepath.Join(cwd, base)
+	}
+	return getName(base), fpath
+}
+
+type Cli struct {
+	rootCmd *cobra.Command
+	cmd     *cmd.Runner
+}
+
+func NewCli() *Cli {
+	SetWorkDir()
+	c := &Cli{
+		rootCmd: &cobra.Command{
+			Short: "asciinema terminal recorder.",
+			Long:  "acast <Command> <SubCommand> --flags args...",
+		},
+		cmd: cmd.New(),
+	}
+	c.rootCmd.AddGroup(&cobra.Group{ID: GroupID, Title: "Command list: "})
+	c.initiate()
+	return c
+}
+
+func (c *Cli) initiate() {
+	if c.rootCmd == nil || c.cmd == nil {
+		return
+	}
+
+	// Auth to aciinema.org
+	auth := &cobra.Command{
+		Use:     "auth",
+		Aliases: []string{"a"},
+		GroupID: GroupID,
+		Short:   "Authrization to asciinema.org.",
+		Run: func(cc *cobra.Command, args []string) {
+			authUrl, info := c.cmd.Auth()
+			gprint.PrintInfo(info)
+			var cmd *exec.Cmd
+			if runtime.GOOS == gutils.Darwin {
+				cmd = exec.Command("open", authUrl)
+			} else if runtime.GOOS == gutils.Linux {
+				cmd = exec.Command("x-www-browser", authUrl)
+			} else if runtime.GOOS == gutils.Windows {
+				cmd = exec.Command("cmd", "/c", "start", authUrl)
+			} else {
+				gprint.PrintError("unsupported os")
+			}
+
+			if err := cmd.Run(); err != nil {
+				gprint.PrintError("auth failed: %+v", err)
+			}
+		},
+	}
+	c.rootCmd.AddCommand(auth)
+
+	// Record.
+	record := &cobra.Command{
+		Use:     "record",
+		Aliases: []string{"r"},
+		Short:   "Creates a record.",
+		Long:    "Example: acast record <xxx.cast>",
+		Run: func(cc *cobra.Command, args []string) {
+			if len(args) == 0 {
+				cc.Help()
+				return
+			}
+			c.cmd.Title, c.cmd.FilePath = handleFilePath(args[0])
+			err := c.cmd.Rec()
+			if err != nil {
+				gprint.PrintError("record failed: %+v", err)
+			}
+		},
+	}
+	c.rootCmd.AddCommand(record)
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gvcgo/goutils v0.8.9
 	github.com/olivere/ndjson v0.1.3
 	github.com/pkg/errors v0.9.1
+	github.com/spf13/cobra v1.8.0
 	golang.org/x/net v0.20.0
 	golang.org/x/sys v0.16.0
 	golang.org/x/term v0.16.0
@@ -20,6 +21,7 @@ require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.8.0 // indirect
 	github.com/gogf/gf/v2 v2.6.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/pty v1.1.8 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -27,6 +29,7 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	go.opentelemetry.io/otel v1.15.1 // indirect
 	go.opentelemetry.io/otel/trace v1.15.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
